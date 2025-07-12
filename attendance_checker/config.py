6d6c5b9261b78c013c910f1ddf588df6182c0117
duvi1234 <durgapc35@gmail.com>
2025-07-12 16:52:30 +0530

    Update config.py

--- attendance_checker/config.py
+++ attendance_checker/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = 'h8snZ2_NNi1ARAyMywELq1pNzNTC1I7riSzTRomgTQg'
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'instance', 'attendance.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
